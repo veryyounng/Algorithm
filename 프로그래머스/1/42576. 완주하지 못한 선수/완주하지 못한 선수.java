aeea9ea300972ab1762f27074c558a44343242b3
소영 <121228672+veryyounng@users.noreply.github.com>
2024-01-05 00:08:21 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 68.81 ms, Memory: 95.8 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,17 +1,25 @@
 import java.util.*;
+
 class Solution {
     public String solution(String[] participant, String[] completion) {
         
-        Arrays.sort(participant);
-        Arrays.sort(completion);
+        //23:12
+         Map<String, Integer> map = new HashMap<>();
         
-        for(int i = 0; i < completion.length; i++){
-            if(participant[i].equals(completion[i])){
-                continue;
-            }else{
-                return participant[i];
+        for(String str : participant){
+            map.put(str, map.getOrDefault(str, 0) + 1);
+        }
+        
+        for(String str : completion){
+            map.put(str, map.get(str) - 1);
+        }
+        
+        for(String key : map.keySet()){
+            if(map.get(key) != 0){
+                return key;
             }
         }
-        return participant[participant.length-1];
+        
+        return null;
     }
 }
