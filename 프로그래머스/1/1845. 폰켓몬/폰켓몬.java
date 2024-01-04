6cbc618d2fcc29b590b7d84724e2584df2499573
소영 <121228672+veryyounng@users.noreply.github.com>
2024-01-04 15:33:12 +0900

    [level 1] Title: 폰켓몬, Time: 4.13 ms, Memory: 79.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        
+        int max = nums.length / 2;
+        HashSet<Integer> set = new HashSet<>();
+        
+        for(int i : nums){
+            set.add(i);
+        }
+        
+        if(set.size() > max){
+            return max;
+        }else{
+            return set.size();
+        }
+        
+    }
+}
