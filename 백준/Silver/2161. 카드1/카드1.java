c765151f1d2d6265a90ed32553594a53a191de1d
소영 <121228672+veryyounng@users.noreply.github.com>
2024-01-08 19:48:32 +0900

    [Silver V] Title: 카드1, Time: 272 ms, Memory: 18648 KB -BaekjoonHub

---
+++ 백준/Silver/2161. 카드1/카드1.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Queue<Integer> q = new LinkedList<>();
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        
+        for(int i = 1; i <= n; i++){
+            q.add(i);
+        }
+        while(q.size() != 1){
+            int s = q.poll();
+            System.out.print(s + " ");
+            q.add(q.poll());
+        }
+        System.out.print(q.poll());
+    }
+}
