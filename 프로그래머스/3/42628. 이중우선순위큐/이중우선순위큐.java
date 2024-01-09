e3982364fbe3edc4885e62aaf87faa2e407aab19
소영 <121228672+veryyounng@users.noreply.github.com>
2024-01-10 02:20:59 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.62 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] operations) {
+        int[] answer = {};
+        PriorityQueue<Integer> min = new PriorityQueue<>();
+        PriorityQueue<Integer> max = new PriorityQueue<>(Collections.reverseOrder());
+        StringTokenizer st;
+        
+        for(int i = 0; i < operations.length; i++){
+            st = new StringTokenizer(operations[i]);
+            char c = st.nextToken().charAt(0);
+            int num = Integer.parseInt(st.nextToken());
+        
+        switch(c){
+            case 'I':
+                min.add(num);
+                max.add(num);
+                break;
+            
+            case 'D':
+                if(max.isEmpty()) break;
+                if(num == 1){
+                    int d = max.poll();
+                    min.remove(d);
+                }
+                if(num == -1){
+                    int d = min.poll();
+                    max.remove(d);
+                }
+        }
+      }
+        System.out.println(min);
+        if(max.isEmpty()){
+            return new int[] {0,0};
+        }
+        return new int[] {max.peek(), min.peek()};
+
+    }
+}
