99f5aa690d02294d8f8fc9fd3660120715c27a7e
소영 <121228672+veryyounng@users.noreply.github.com>
2024-01-15 18:53:11 +0900

    [level Lv.0] Title: 이어 붙인 수, Time: 12.67 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/Lv.0/181928. 이어 붙인 수/이어 붙인 수.java
+++ 프로그래머스/Lv.0/181928. 이어 붙인 수/이어 붙인 수.java
@@ -1,6 +1,5 @@
 class Solution {
     public int solution(int[] num_list) {
-        int answer = 0;
         String odd = "";
         String even = "";
         
@@ -10,7 +9,6 @@ class Solution {
             else
                 even += "" + num_list[i];
         }
-        answer = Integer.parseInt(odd) + Integer.parseInt(even);
-        return answer;
+        return Integer.parseInt(odd) + Integer.parseInt(even);
     }
 }
