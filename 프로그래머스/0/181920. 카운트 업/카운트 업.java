9c28a57323fa6f1dd9215880a4dbc27f2a620d9f
소영 <121228672+veryyounng@users.noreply.github.com>
2024-01-17 00:03:13 +0900

    [level 0] Title: 카운트 업, Time: 0.03 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/0/181920. 카운트 업/카운트 업.java
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.java
@@ -2,16 +2,11 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int start_num, int end_num) {
-        int le = end_num - start_num + 1;
-        ArrayList<Integer> li = new ArrayList<>();
         
-        int[] answer = new int[le];
-        
-        for(int i = start_num; i <= end_num; i++){
-            li.add(i);
-        }
-        for(int i = 0; i < li.size(); i++){
-            answer[i] = li.get(i);
+        int[] answer = new int[end_num - (start_num - 1)];
+        for(int i = 0; i < answer.length; i++){
+            answer[i] = start_num;
+            start_num++;
         }
         return answer;
     }
