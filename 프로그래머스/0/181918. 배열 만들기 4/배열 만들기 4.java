97171b8554dfe43be6050df5a43a7611036a8380
소영 <121228672+veryyounng@users.noreply.github.com>
2024-01-19 00:14:32 +0900

    [level 0] Title: 배열 만들기 4, Time: 5.16 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.java
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.java
@@ -1,25 +1,17 @@
 import java.util.*;
+
 class Solution {
-    public int[] solution(int[] arr) {
-        ArrayList<Integer> li = new ArrayList<>();
-        int i = 0; 
+    public Stack<Integer> solution(int[] arr) {
+        int i = 0;
+        Stack<Integer> stack = new Stack<Integer>();
         while(i < arr.length){
-            if(li.size() == 0){
-                li.add(arr[i]);
+            if(stack.empty() || stack.peek() < arr[i]){
+                stack.push(arr[i]);
                 i++;
-            }else if( li.get(li.size() - 1) < arr[i]){
-                li.add(arr[i]);
-                i++;
-            }else if(li.get(li.size() - 1 ) >= arr[i]){
-                li.remove(li.size() - 1);
+            }else if(stack.peek() >= arr[i]){
+                stack.pop();
             }
         }
-        
-        int[] stk = new int[li.size()];
-        for(int j = 0; j < li.size(); j++){
-            stk[j] = li.get(j);
-        }
-        
-        return stk;
+        return stack;
     }
 }
