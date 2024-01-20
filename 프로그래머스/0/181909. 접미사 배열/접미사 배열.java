8f62783659569baaa8d08090961b653ff7d856e9
소영 <121228672+veryyounng@users.noreply.github.com>
2024-01-20 23:43:53 +0900

    [level 0] Title: 접미사 배열, Time: 0.23 ms, Memory: 78.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String my_string) {
+        int length = my_string.length();
+        String[] answer = new String[length];
+        for(int i = 0; i < length; i++){
+            answer[i] = my_string.substring(i);
+        }
+        Arrays.sort(answer);
+        return answer;
+    }
+}
