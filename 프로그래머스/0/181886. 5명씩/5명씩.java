ce53ee0fb1032e20536af7b67bb1aa73bbdd0f5f
소영 <121228672+veryyounng@users.noreply.github.com>
2024-01-25 21:46:54 +0900

    [level 0] Title: 5명씩, Time: 0.02 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/0/181886. 5명씩/5명씩.java
+++ 프로그래머스/0/181886. 5명씩/5명씩.java
@@ -1,16 +1,10 @@
-import java.util.*;
-
 class Solution {
     public String[] solution(String[] names) {
-        ArrayList<String> li = new ArrayList<>();
-        for(int i= 0; i < names.length; i++){
-            if(i % 5 == 0){
-                li.add(names[i]);
-            }
-        }
-        String[] answer = new String[li.size()];
-        for(int i = 0; i < li.size(); i++){
-            answer[i] = li.get(i);
+        int idx = 0;
+        
+        String[] answer = new String[names.length % 5 == 0? names.length/5 : names.length/5 + 1];
+        for(int i = 0; i < names.length; i+=5){
+            answer[idx++] = names[i];
         }
         return answer;
     }
