7b40e6bb165393063ed8b5cacaa199218ab99bac
소영 <121228672+veryyounng@users.noreply.github.com>
2024-01-27 18:41:24 +0900

    [level 0] Title: 원하는 문자열 찾기, Time: 5.25 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.java
+++ 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.java
@@ -1,25 +1,9 @@
 class Solution {
     public int solution(String myString, String pat) {
-        String a = "";
-        String b = "";
-        for(int i = 0; i < myString.length(); i++){
-            if(Character.isUpperCase(myString.charAt(i))){
-                a += (char)(myString.charAt(i));
-            }else{
-                a += (char)(myString.charAt(i) - 32);
-            }
-        }
-        for(int i = 0; i < pat.length(); i++){
-            if(Character.isUpperCase(pat.charAt(i))){
-                b += (char)(pat.charAt(i));
-            }else{
-                b += (char)(pat.charAt(i) - 32);
-            }
-        }
+        String a = myString.toUpperCase();
+        String b = pat.toUpperCase();
         if(a.contains(b)){
             return 1;
-        }else{
-            return 0;
-        }
+        }return 0;
     }
 }
