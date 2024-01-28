4c03f144a5606ffa063e0b33fedc118a2305acbf
소영 <121228672+veryyounng@users.noreply.github.com>
2024-01-28 22:25:54 +0900

    [level 0] Title: A 강조하기, Time: 0.08 ms, Memory: 66.8 MB -BaekjoonHub

--- 프로그래머스/0/181874. A 강조하기/A 강조하기.java
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.java
@@ -1,17 +1,7 @@
 class Solution {
     public String solution(String myString) {
-        String answer = "";
-        for(char c :  myString.toCharArray()){
-            if(c == 'a'){
-                String tmp = String.valueOf(c);
-                answer += tmp.toUpperCase();
-            }else if(c != 'A' && c != 'a'){
-                String tmp = String.valueOf(c);
-                answer += tmp.toLowerCase();
-            }else if(c == 'A'){
-                answer += c;
-            }
-        }
+        String answer = myString.toLowerCase();
+        answer = answer.replaceAll("a", "A");
         return answer;
     }
 }
