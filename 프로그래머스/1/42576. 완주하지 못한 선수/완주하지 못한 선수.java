8b82e90fa936b8e477144aeeaf56b0d07072a3a4
소영 <121228672+veryyounng@users.noreply.github.com>
2024-05-21 23:31:13 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 345.33 ms, Memory: 95.3 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,27 +1,16 @@
 import java.util.*;
-
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        
-        //23:12
-        //string, integer map 만들기
-        //participant 빠른 for문 돌려서 getOrDefault str 있으면 +1, 없으면 0
-        //completion 빠른 for문 돌려서 map.get해서 -1 
-        //map에서 value가 0이 아니면 return
-        
-         Map<String, Integer> map = new HashMap<>();
-        
-        for(String str : participant){
-            map.put(str, map.getOrDefault(str, 0)+1);
-        }
-        for(String str : completion){
-            map.put(str,  map.get(str)-1);
-        }
-        for(String key: map.keySet()){
-            if(map.get(key)!= 0){
-                return key;
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+        String answer = "";
+        int i;
+        for(i = 0; i < completion.length; i++){
+            if(!participant[i].equals(completion[i])){
+                return participant[i];
             }
+            
         }
-        return null;
+        return participant[i];
     }
 }
