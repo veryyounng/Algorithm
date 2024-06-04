aa890f6c719d9091d5bfd7f807a7d31fdfc0429c
소영 <121228672+veryyounng@users.noreply.github.com>
2024-06-04 23:57:47 +0900

    [level 1] Title: 체육복, Time: 0.36 ms, Memory: 72.3 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,32 +1,39 @@
-import java.util.*;
+import java.util.Arrays;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        int[] clothes = new int[n];
-        Arrays.fill(clothes, 1);
-        for(int l : lost){
-            clothes[l-1]--;
-        }
-        for(int r : reserve){
-            clothes[r-1]++;
-        }
-        for(int i = 0; i < clothes.length; i++){
-            if(clothes[i] == 0){
-                if(i-1 >= 0 && clothes[i-1] == 2){
-                    clothes[i] ++;
-                    clothes[i-1] --;
-                }else if(i+1 < n && clothes[i+1] == 2){
-                    clothes[i] ++;
-                    clothes[i+1] --;
-                }
-            }
-        }
         int answer = 0;
-        for(int c : clothes){
-            if(c > 0){
-                answer ++;
-            }
-        }
+        
+        Arrays.sort(reserve);
+        Arrays.sort(lost);
+        
+        // 도난 당하지 않은 학생 수
+        answer = n - lost.length;
+        
+        // 여벌 체육복을 가져왔지만 도난당한 학생 수
+        // 다른 학생에게 체육복을 빌려줄 수 없음
+        for (int i = 0; i < lost.length; i++) {
+			for (int j = 0; j < reserve.length; j++) {
+				if (lost[i] == reserve[j]) {
+					answer++;
+					lost[i] = -1;
+					reserve[j] = -1;
+                    break;
+				}
+			}
+		}
+        
+        // 도난당했지만 체육복을 빌릴 수 있는 학생 수
+        for (int i = 0; i < lost.length; i++) {
+			for (int j = 0; j < reserve.length; j++) {
+				if (lost[i] - 1 == reserve[j] || lost[i] + 1 == reserve[j]) {
+					answer++;
+					reserve[j] = -1;
+					break;
+				}
+			}
+		}
+        
         return answer;
     }
 }
