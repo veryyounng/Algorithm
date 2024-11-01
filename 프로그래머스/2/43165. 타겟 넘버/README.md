d69ac3be8d62cd75e8083940354509a81188209d
소영 <121228672+veryyounng@users.noreply.github.com>
2024-11-02 08:24:37 +0900

    [level 2] Title: 타겟 넘버, Time: 5.21 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=java) 
 
 ### 성능 요약
 
-메모리: 72.7 MB, 시간: 0.35 ms
+메모리: 75.8 MB, 시간: 5.21 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 22일 05:13:00
+2024년 11월 02일 08:24:34
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,13 +1,22 @@
-class Solution {
+import java.util.*;
+
+public class Solution {
     public int solution(int[] numbers, int target) {
-        return find(numbers, target, 0, 0);
+        return dfs(numbers, target, 0, 0);
     }
-    private int find(int[] numbers, int target, int index, int currentSum){
-        if(index == numbers.length){
-            return currentSum == target? 1 : 0;
+    
+    private int dfs(int[] numbers, int target, int index, int sum) {
+        // 모든 숫자를 탐색했을 때
+        if (index == numbers.length) {
+            // 최종 합이 타겟과 같으면 1을 반환
+            return sum == target ? 1 : 0;
         }
-        int add = find(numbers, target, index+1, currentSum + numbers[index]);
-        int subStract = find(numbers, target, index+1, currentSum - numbers[index]);
-        return add + subStract;
+        
+        // 현재 숫자를 더하거나 빼는 두 가지 경우를 재귀적으로 호출
+        int add = dfs(numbers, target, index + 1, sum + numbers[index]);
+        int subtract = dfs(numbers, target, index + 1, sum - numbers[index]);
+        
+        // 두 경우의 수를 합산하여 반환
+        return add + subtract;
     }
 }
