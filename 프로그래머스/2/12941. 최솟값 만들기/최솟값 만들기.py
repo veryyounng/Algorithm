93be7bca4295a41e61493c7832113343e1c8ffd3
소영 <121228672+veryyounng@users.noreply.github.com>
2025-01-07 22:00:36 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.22 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.py
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.py
@@ -1,10 +1,11 @@
-def solution(A, B):
-    # A는 오름차순 정렬
+def solution(A,B):
+    answer = 0
+
     A.sort()
-    # B는 내림차순 정렬
-    B.sort(reverse=True)
+    B.sort(reverse = True)
     
-    # 각 인덱스의 값을 곱한 결과를 합산
-    answer = sum(a * b for a, b in zip(A, B))
-    
+    for a, b in zip(A, B):
+        product = a * b
+        answer += product
+
     return answer
