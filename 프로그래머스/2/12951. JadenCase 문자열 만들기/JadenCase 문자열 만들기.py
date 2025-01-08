e8962ba4eed547702703e562f8fa91cda08fec06
소영 <121228672+veryyounng@users.noreply.github.com>
2025-01-08 21:34:41 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -0,0 +1,9 @@
+def solution(s):
+    answer = [];
+    words = s.split(" ");
+    for word in words:
+        if word:
+            answer.append(word[0].upper() + word[1:].lower());
+        else:
+            answer.append(word);
+    return " ".join(answer);
