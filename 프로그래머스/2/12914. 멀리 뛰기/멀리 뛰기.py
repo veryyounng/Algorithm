e0f1b24a726357199f8d893bc13102dc1047b835
소영 <121228672+veryyounng@users.noreply.github.com>
2025-01-21 22:45:02 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.83 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -1,13 +1,9 @@
 def solution(n):
-    # n칸을 뛰는 방법을 저장할 배열 선언
-    dp = [0] * (n + 1)
-    # 초기 값 설정
-    dp[0] = 1  # 0칸일 때 경우의 수
-    dp[1] = 1  # 1칸일 때 경우의 수
+    dp = [0] * (n+1)
+    dp[0] = 1
+    dp[1] = 1
     
-    # 점화식을 이용해 DP 배열 계산
-    for i in range(2, n + 1):
-        dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567
-    
-    # 정답 반환
-    return dp[n]
+    for i in range(2, n+1):
+        dp[i] = (dp[i-2] + dp[i-1])
+        
+    return dp[n] % 1234567
