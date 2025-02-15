d3dee1c95d66c330bf0d9b400728ceac70402a6b
소영 <121228672+veryyounng@users.noreply.github.com>
2025-02-15 16:09:09 +0900

    [level 3] Title: 네트워크, Time: 0.67 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,17 +1,19 @@
 def solution(n, computers):
     answer = 0
-
+    
+    visited = [0] * n
+    
     def DFS(i):
         visited[i] = 1
+        
         for a in range(n):
             if not visited[a]:
                 if computers[i][a]:
                     DFS(a)
                     
-                    
-    visited = [0] * n
     for i in range(n):
         if not visited[i]:
             DFS(i)
             answer += 1
+            
     return answer
