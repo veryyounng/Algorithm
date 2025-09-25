845a39e96ae6c58f87462b947ba77437d95254a6
최소영 <121228672+veryyounng@users.noreply.github.com>
2025-09-26 08:11:28 +0900

    [Silver I] Title: 숨바꼭질, Time: 96 ms, Memory: 38148 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -0,0 +1,19 @@
+from collections import deque
+
+n, k = map(int, input().split())
+
+queue = deque([n])
+max = 100001
+visited = [0] * (max)
+
+def BFS(v):
+    while queue:
+        v = queue.popleft()
+        if v == k:
+            return visited[v]
+        for next in (v-1, v+1, v*2):
+            if next >=0 and next < max and visited[next] == 0:
+                visited[next] = visited[v] + 1
+                queue.append(next)
+
+print(BFS(n))
